2f4e26158f9aa9257d2abca0cf1bc2bc889a4f58
manishbhashkar123 <49497153+manishbhashkar123@users.noreply.github.com>
2019-06-05 17:45:03 +0530

    Update settings.py

--- curddPro/curdPro/settings.py
+++ curddPro/curdPro/settings.py
@@ -78,10 +78,8 @@ WSGI_APPLICATION = 'curdPro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'curddb6pm',
-        'USER':'root',
-        'PASSWORD':'root'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),     
     }
 }
 
